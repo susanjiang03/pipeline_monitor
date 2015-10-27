5c046e6b648c9ff8a76d4aba725753092033f8ff
James Hohman <jhohman@citymail.cuny.edu>
2015-10-27 16:48:22 -0400

    Initial models and admin.

--- site/pmonitor/admin.py
+++ site/pmonitor/admin.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+from pmonitor.models import Job, Task
+
 __author__ = 'jhohman'
+
+
+admin.site.register(Job)
+admin.site.register(Task)
+
--- site/pmonitor/models.py
+++ site/pmonitor/models.py
@@ -4,17 +4,22 @@ from datetime import datetime
 
 __author__ = 'jhohman'
 
-# ToDo: Models...
-# class Task(models.Model):
-#     parent = models.ForeignKey("self")
-#     child = models.ForeignKey("self")
-#     siblings = models.ForeignKey("self")
-#
-#
-# class Pipeline(models.Model):
-#     """
-#     A pipeline is a parent object that holds a series of tasks.
-#     """
-#     user = models.ForeignKey(User)
-#     date = models.DateTimeField(default=datetime.now())
-#     tasks = models.ManyToManyField(Task)
+class Task(models.Model):
+    parent = models.ForeignKey("self", related_name='task_parents', null=True, blank=True)
+    child = models.ForeignKey("self", related_name='task_children', null=True, blank=True)
+    siblings = models.ForeignKey("self", related_name='task_siblings', null=True, blank=True)
+    task_id = models.CharField(blank=True, max_length=64)
+    task_name = models.CharField(blank=True, max_length=256)
+    task_description = models.CharField(blank=True, max_length=1024)
+
+
+class Job(models.Model):
+    """
+    A job (a pipeline) is a parent object that holds a series of tasks.
+    """
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(default=datetime.now())
+    entry_task = models.ForeignKey(Task, null=True, blank=True)
+    description = models.CharField(max_length=1024, blank=True)
+    name = models.CharField(max_length=256, blank=True)
+
