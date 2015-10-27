0a2c3b505c97f54a8ff7caf52061b1c43c0f31c1
James Hohman <jhohman@citymail.cuny.edu>
2015-10-27 16:05:25 -0400

    Added bootstrap 3 to project.

--- site/core/settings.py
+++ site/core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pmonitor'
+    'pmonitor',
+    'bootstrap3'
 )
 
 MIDDLEWARE_CLASSES = (
