10d347823d16f081ff85500745c928fa3dd5a3b6
James Hohman <jhohman@citymail.cuny.edu>
2015-10-27 16:19:15 -0400

    Added dismissible bootstrap messages, via django message framework.

--- site/pmonitor/views/index.py
+++ site/pmonitor/views/index.py
@@ -1,5 +1,7 @@
 import logging
 from django.shortcuts import render
+from django.contrib import messages
+from random import randint
 
 log = logging.getLogger(__name__)
 
@@ -9,6 +11,6 @@ __author__ = 'jhohman'
 def index(request):
     log.info('index view')
     template = 'pmonitor/index.html'
-    print 'root'
-    print template
+    messages.info(request, "I'm a server generated random int! %d" % randint(1, 100))
+    messages.warning(request, "Try to dismiss this warning!")
     return render(request, template)
