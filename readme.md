b222d85d4a7af1a5d445b8bed3c6fe0dd26eaed6
susanjiang03 <susanjiang03@gmail.com>
2015-11-05 08:09:30 -0500

    a example of detail of rss feed from a newspaper site

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Pipeline Monitor (pmonitor)
+### A Django Site to Monitor Processes
+
+## Purpose
+This application will monitor arbitrary processes and display 
+visualizations of the process state. 
+
+## Version Info
+Version: 0.01
+
+Date: 10/26/2015
+
+## Authors
+ * Daniel Chia
+ * Eddie Chan
+ * James Hohman
+ * Susan Jiang
+
+
+## Requirements
+ * Python 2.7.10
+ * Django 1.8.5
+
+
+## License, Copyright
+License should go here.
+
+## Main packages
+| path | description |
+-------|--------------
+| site/ | contains the django site. |
+| simulator/ | contains a simple process that can be monitored. |
+ 
---
+++ site/pmonitor/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+__author__ = 'jhohman'
+
+class Task(models.Model):
+    parent = models.ForeignKey("self", related_name='parent_of', null=True, blank=True)
+    child = models.ForeignKey("self", related_name='child_of', null=True, blank=True)
+    siblings = models.ManyToManyField("self", blank=True)
+    task_id = models.CharField(blank=True, max_length=64)
+    task_name = models.CharField(blank=True, max_length=256)
+    task_description = models.CharField(blank=True, max_length=1024)
+
+    def __str__(self):
+        return '<Task %s>' % self.task_id
+
+    def save(self, *args, **kwargs):
+        import ipdb;ipdb.set_trace()
+        super(Task, self).save(*args, **kwargs)
+
+
+class Job(models.Model):
+    """
+    A job (a pipeline) is a parent object that holds a series of tasks.
+    """
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(default=datetime.now)
+    entry_task = models.ForeignKey(Task, null=True, blank=True)
+    description = models.CharField(max_length=1024, blank=True)
+    name = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return '<Job %s, user: %s>' % (self.name, self.user.get_username())
+
