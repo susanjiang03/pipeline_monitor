5b884b7934fef645486b3e4fbb2b77eae42c7d3c
Daniel Chia <dchia00@citymail.cuny.edu>
2015-11-09 12:15:06 -0500

    cleared model nonsense

--- site/news/admin.py
+++ site/news/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from news.models import Newspaper, Category, Article
+from news.models import Article
+
+#customize admin
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'news_slug':('newspaper',),
+                           'cat_slug':('category',)}
+
 
 # Register your models here.
-admin.site.register(Newspaper)
-admin.site.register(Category)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
--- site/news/models.py
+++ site/news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 '''
 class RSS(models.Model):
     title = models.CharField(max_length=200)
@@ -12,35 +12,25 @@ class RSS(models.Model):
         return self.title
 '''
 
-#3 main classes, newspaper, category and articles
-class Newspaper(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-
-    #will need to create slug here, for the urls
-
-    def __unicode__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-
-    def __unicode__(self):
-        return self.name
-
-
+#in order to create urls, newspaper and categories can be slugified
 class Article(models.Model):
     #Newspaper | Category | Title | Link | Description
-    newspaper = models.ForeignKey(Newspaper)
-    category = models.ForeignKey(Category)
+    newspaper = models.CharField(max_length=20) 
+    category = models.CharField(max_length=15)
     title = models.CharField(max_length=200)
     url = models.URLField()
     description = models.CharField(max_length=200)
+    #2 slug fields for urls
+    news_slug = models.SlugField(unique=True)
+    cat_slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.news_slug = slugify(self.newspaper)
+        self.cat_slug = slugify(self.category)
+        super(Article, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.title
-
-
 
 
 
