7563ecbc865abaffa4ee43afbeb0537137cf43f2
Daniel Chia <dchia00@citymail.cuny.edu>
2015-11-10 02:54:05 -0500

    db has necessary attributes and can start taking queries

--- site/populate_articles.py
+++ site/populate_articles.py
@@ -20,8 +20,11 @@ def populate():
     #two if checks
 
     #create dictionary here to match the patterns
-    #nytimes - New York times to insert into title
     newspapers = {'nytimes': 'New York Times', 'latimes': 'Los Angeles Times', 'miamiherald': 'Miami Herald', 'seattletimes':'Seattle Times', 'chron':'Houston Chronicles', 'denverpost':'Denver Post'}
+
+    #only need a simple list here
+    categories = ['world', 'business', 'technology', 'fashion', 'sports']
+
 
     #insert list of keywords that belong to a category
     for url in urls:
@@ -45,14 +48,26 @@ def populate():
             #clean it with regex
             clean_descr = re.sub(r'<[^>]*>', '', descr) 
 
-            newspaper = ''
             #match up the newspaper title
             for key in newspapers:
                 if key in url:
                     newspaper = newspapers[key] 
-   
-            article = add_article(newspaper, "nada", title, link, clean_descr)
-            
+
+
+            #find the category
+            category = ''
+            for cat in categories:
+                #found category
+                #this logic sucks, at the end it doesnt match so it ends up putting local
+                if cat in url.lower():
+                    category = cat.capitalize()
+
+            #if category was not found, its local
+            if category.lower() not in categories:
+                category = 'Local'
+             
+            article = add_article(newspaper, category, title, link, clean_descr)
+
 
 #add article, pass newspaper, category, title, url, description
 def add_article(news, cat, title, url, desc):
