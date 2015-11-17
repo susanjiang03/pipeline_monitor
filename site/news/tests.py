7e5e7805ae9a88794442e05733a5795382917350
Daniel Chia <dchia00@citymail.cuny.edu>
2015-11-17 15:13:22 -0500

    added initial tests.py for news app

---
+++ site/news/tests.py
@@ -0,0 +1,52 @@
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from news.views import index, filterfeeds, userfeeds, newspaper
+from news.models import Article
+
+#what to test on the home page?
+
+class MainPageTest(TestCase):
+
+    def test_main_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        expected_html = render_to_string('index.html')
+        #this doesn't test anything atm
+
+    #test that we have the 6 newspaper categories
+    def test_main_page_has_six_categories(self):
+        categories = ['World', 'Local', 'Technology', 'Business', 'Sports', 'Fashion'] 
+        expected_html = render_to_string('index.html').encode('utf-8')
+
+        #currently fails because it checks the template, not the populated page
+
+    #test that we have 6 newpapers on the side
+
+
+class AritcleModeltest(TestCase):
+
+    def test_saving_and_retrieving_articles(self):
+
+        #article has title | url | description | newspaper | category
+        article_1 = Article()
+        article_1.newspaper = "Python Daily"
+        article_1.category = "Programming"
+        article_1.title = "Python in 10 minutes"
+        article_1.url = "http://realpython.com"
+        article_1.description = "Learn the Python programming language in 10 minutes"
+        #gotta know the format before testing date
+        article_1.save()
+
+        #test 5 fields
+        self.assertEqual(article_1.newspaper, 'Python Daily')
+        self.assertEqual(article_1.category, 'Programming')
+        self.assertEqual(article_1.title, 'Python in 10 minutes')
+        self.assertEqual(article_1.url, "http://realpython.com")
+        self.assertEqual(article_1.description, "Learn the Python programming language in 10 minutes")
+        
+
+
+
