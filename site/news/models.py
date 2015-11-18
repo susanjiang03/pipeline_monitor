38156b8c70e5f2122a3aef310f3d64d2808706be
Daniel Chia <dchia00@citymail.cuny.edu>
2015-11-18 13:16:02 -0500

    added coverage and pylint as dependencies in requirements.txt, updated model to use textfields

--- site/news/models.py
+++ site/news/models.py
@@ -6,9 +6,9 @@ class Article(models.Model):
     #Newspaper | Category | Title | Link | Description
     newspaper = models.CharField(max_length=20) 
     category = models.CharField(max_length=15)
-    title = models.CharField(max_length=200)
+    title = models.TextField(default='')
     url = models.URLField()
-    description = models.CharField(max_length=200)
+    description = models.TextField(default='')
     publish_date = models.DateTimeField(default=datetime.now)
 
     def __unicode__(self):
