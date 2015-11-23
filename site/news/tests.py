44055d0e01451f8a9c831ac10789853e9134ef67
Daniel Chia <dchia00@citymail.cuny.edu>
2015-11-22 22:41:44 -0500

    refactored news and newspaper views tests

--- site/news/tests.py
+++ site/news/tests.py
@@ -6,23 +6,31 @@ from django.template.loader import render_to_string
 from news.views import index, filterfeeds, userfeeds, newspaper
 from news.models import Article
 
-#what to test on the home page?
+#news page
+class NewsPageTest(TestCase):
 
-class MainPageTest(TestCase):
+    #checks that /news/ goes to index view
+    def test_news_url_resolves_to_index_view(self):
+        found = resolve('/news/')
+        self.assertEqual(found.func, index)
 
-    def test_main_page_returns_correct_html(self):
+    #checks that index view has the index html template
+    def test_news_page_returns_correct_html(self):
         request = HttpRequest()
         response = index(request)
         expected_html = render_to_string('index.html')
-        #this doesn't test anything atm
+        self.assertEqual(response.content.decode(), expected_html)
 
-    #test that we have the 6 newspaper categories
-    def test_main_page_has_six_categories(self):
-        categories = ['World', 'Local', 'Technology', 'Business', 'Sports', 'Fashion']
-        expected_html = render_to_string('index.html').encode('utf-8')
+    #seems like I have to create the articles manually
+    #every damn time
+    def test_article_appears_on_news_page(self):
+        article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
 
-        #currently fails because it checks the template, not the populated page
+        response = self.client.get('/news/')
+        self.assertContains(response, 'Technology')
+        self.assertContains(response, 'New York Times')
 
+#article model
 class AritcleModeltest(TestCase):
 
     def test_saving_and_retrieving_articles(self):
@@ -34,10 +42,19 @@ class AritcleModeltest(TestCase):
         article_1.title = "Python in 10 minutes"
         article_1.url = "http://realpython.com"
         article_1.description = "Learn the Python programming language in 10 minutes"
-        #gotta know the format before testing date
         article_1.save()
 
-        #test 5 fields
+        article_2 = Article()
+        article_2.newspaper = "NYT"
+        article_2.category = "Programming"
+        article_2.title = "Kung Fu React"
+        article_2.url = "http://flask.com"
+        article_2.description = "Best tutorial to learning react on the web"
+        article_2.save()
+
+        saved_articles = Article.objects.all()
+        self.assertEqual(saved_articles.count(), 2)
+
         self.assertEqual(article_1, Article.objects.all()[0])
         self.assertEqual(article_1.newspaper, 'Python Daily')
         self.assertEqual(article_1.category, 'Programming')
@@ -45,19 +62,36 @@ class AritcleModeltest(TestCase):
         self.assertEqual(article_1.url, "http://realpython.com")
         self.assertEqual(article_1.description, "Learn the Python programming language in 10 minutes")
 
-    def create_article(self, blah='hello, I like water'):
-        return Article.objects.create(title=blah, url=blah, description=blah, 
-                                      category=blah, newspaper=blah)
-
-    def test_article_creation(self):
-        a = self.create_article()
-        self.assertTrue(isinstance(a, Article))
- 
-        self.assertEqual(a.__unicode__(), a.title)
+        self.assertEqual(article_2, Article.objects.all()[1])
+        self.assertEqual(article_2.newspaper, "NYT")
+        self.assertEqual(article_2.category, "Programming")
+        self.assertEqual(article_2.title, "Kung Fu React")
+        self.assertEqual(article_2.url, "http://flask.com")
+        self.assertEqual(article_2.description, "Best tutorial to learning react on the web")
 
 
+class NewspaperViewTest(TestCase):
 
+    #bad test because, the create of the article newspaper does
+    #not directly affect the url creation, no relation
+    def test_uses_newspaper_template(self):
+        article = Article.objects.create()
+        article.newspaper = "New York Times"
+        response = self.client.get('/news/newspaper/nytimes/')
+        self.assertTemplateUsed(response, 'newspaper.html')
 
+    #can't populate pages for some reason
+    def test_displays_article_from_that_newspaper(self):
+        #create 2 aritcles from different newspaper
+        #newspaper, title, url, description, category
+        correct_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
+
+        response = self.client.get('/news/newspaper/nytimes/')
+        
+        self.assertContains(response, "New York Times")
+        self.assertContains(response, "Technology")
+        self.assertNotContains(response, "Los Angeles Times")
+        self.assertNotContains(response, "Westside gang")
 
 
 
