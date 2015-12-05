fc4a0747a5513436bbb2858d226880ebda312d27
James Hohman <jhohman00@citymail.cuny.edu>
2015-12-05 20:56:04 +0000

    Added better fl text positioning and overflow clipping.

--- site/pmonitor/static/pmonitor/js/d3_node_chart.js
+++ site/pmonitor/static/pmonitor/js/d3_node_chart.js
@@ -35,13 +35,13 @@ $(document).ready(function() {
     var color = d3.scale.category20();
 
     var force = d3.layout.force()
-        .charge(-2000)
-        .linkDistance(200)
-        .size([width, height]);
+      .charge(-2000)
+      .linkDistance(200)
+      .size([width, height]);
 
     var svg = d3.select("#chart").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+      .attr("width", width)
+      .attr("height", height);
 
     var colors = {
       success: {bc: "lightgreen", bdc: "darkgreen"},
@@ -64,89 +64,109 @@ $(document).ready(function() {
       return;
     }
 
-    var node_dims = { width: 180, height: 84 };
+    var node_dims = {
+      width: 180,
+      height: 84,
+      l_b_factor: 0.05,
+      t_b_factor: 0.2
+    };
 
     force
-        .nodes(graph.nodes)
-        .links(graph.links)
-        .start();
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .start();
 
     var link = svg.selectAll(".link")
-        .data(graph.links)
-        .enter().append("line")
-        .attr("class", "link")
-        .style("stroke-width", function (d) {
-          return Math.sqrt(d.value);
-        });
+      .data(graph.links)
+      .enter().append("line")
+      .attr("class", "link")
+      .style("stroke-width", function (d) {
+        return Math.sqrt(d.value);
+      });
 
     var gnode = svg.selectAll(".node")
-        .data(graph.nodes)
-        .enter()
-        .append("g")
-        .attr("class", "g-nodes");
+      .data(graph.nodes)
+      .enter()
+      .append("g")
+      .attr("class", "g-nodes");
 
-    var node = gnode.append("rect")
-        .attr("class", "node")
+    var clip_path = gnode.append("clipPath")
+      .attr("id", function(d, i) {
+        return "clip-" + i;
+      })
+      .append("rect")
         .attr("width", node_dims.width)
         .attr("height", node_dims.height)
         .attr("rx", 12)
         .attr("ry", 12)
-        .attr("x", -60)
-        .attr("y", -40)
-        .style("fill", function (d) {
-          return colors[d.status]["bc"];
-        })
-        .style("stroke", function (d) {
-          return colors[d.status]["bdc"];
-        })
-        .call(force.drag);
+        .attr("x", -node_dims.width/2)
+        .attr("y", -node_dims.height/2);
 
-    var glabel = gnode.append("g")
+    var node = gnode.append("rect")
+      .attr("class", "node")
+      .attr("width", node_dims.width)
+      .attr("height", node_dims.height)
+      .attr("rx", 12)
+      .attr("ry", 12)
+      .attr("x", -node_dims.width/2)
+      .attr("y", -node_dims.height/2)
+      .style("fill", function (d) {
+        return colors[d.status]["bc"];
+      })
+      .style("stroke", function (d) {
+        return colors[d.status]["bdc"];
+      })
+      .call(force.drag);
+
+    gnode.append("g")
       .each(function (d) {
-        var glabel = d3.select(this);
+        var glabel = d3.select(this)
+        .attr("clip-path", function(d, i) {
+          return "url(#clip-" + i + ")";
+        });
 
         if (d.status === "not run") {
           var text = glabel.append("text")
             .attr("class", "status");
           $.each(["task_id", "name", "status"], function (i, sp) {
             var text = glabel.append("text")
-                .attr("class", "status");
+              .attr("class", "status");
             text.append("tspan")
-                .attr("class", sp + " slabel")
-                .attr("x", -(node_dims.width / 2) + node_dims.width * 0.2)
-                .attr("y", -(node_dims.height / 2) + node_dims.height * 0.2 + 12 * i)
-                .text(sp.replace("_", " ") + ": ");
+              .attr("class", sp + " slabel")
+              .attr("x", -(node_dims.width / 2) + node_dims.width * node_dims.l_b_factor)
+              .attr("y", -(node_dims.height / 2) + node_dims.height * node_dims.t_b_factor + 12 * i)
+              .text(sp.replace("_", " ") + ": ");
 
             if (sp === "url") {
-                text.append("a")
-                    .attr("class", "slink")
-                    .attr("xlink:href", "http://news.yahoo.com")
-                    .text("yahoo news");
+              text.append("a")
+                .attr("class", "slink")
+                .attr("xlink:href", "http://news.yahoo.com")
+                .text("yahoo news");
             } else {
-                text.append("tspan")
-                    .attr("class", "stext")
-                    .text(d[sp]);
+              text.append("tspan")
+                .attr("class", "stext")
+                .text(d[sp]);
             }
           });
         } else {
           $.each(d3_fl_chart.status_properties, function (i, sp) {
             var text = glabel.append("text")
-                .attr("class", "status");
+              .attr("class", "status");
             text.append("tspan")
-                .attr("class", sp + " slabel")
-                .attr("x", -(node_dims.width / 2) + node_dims.width * 0.2)
-                .attr("y", -(node_dims.height / 2) + node_dims.height * 0.2 + 12 * i)
-                .text(sp.replace("_", " ") + ": ");
+              .attr("class", sp + " slabel")
+              .attr("x", -(node_dims.width / 2) + node_dims.width * node_dims.l_b_factor)
+              .attr("y", -(node_dims.height / 2) + node_dims.height * node_dims.t_b_factor + 12 * i)
+              .text(sp.replace("_", " ") + ": ");
 
             if (sp === "url") {
-                text.append("a")
-                    .attr("class", "slink")
-                    .attr("xlink:href", d[sp])
-                    .text("Task details...");
+              text.append("a")
+                .attr("class", "slink")
+                .attr("xlink:href", d[sp])
+                .text("Task details...");
             } else {
-                text.append("tspan")
-                    .attr("class", "stext")
-                    .text(d[sp]);
+              text.append("tspan")
+                .attr("class", "stext")
+                .text(d[sp]);
             }
           });
         }
@@ -154,17 +174,17 @@ $(document).ready(function() {
 
     force.on("tick", function () {
       link.attr("x1", function (d) {
-            return d.source.x;
-          })
-          .attr("y1", function (d) {
-            return d.source.y;
-          })
-          .attr("x2", function (d) {
-            return d.target.x;
-          })
-          .attr("y2", function (d) {
-            return d.target.y;
-          });
+        return d.source.x;
+      })
+        .attr("y1", function (d) {
+          return d.source.y;
+        })
+        .attr("x2", function (d) {
+          return d.target.x;
+        })
+        .attr("y2", function (d) {
+          return d.target.y;
+        });
 
       gnode.attr("transform", function (d) {
         return "translate(" + [d.x, d.y] + ")";
