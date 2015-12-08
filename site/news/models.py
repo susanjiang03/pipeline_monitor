120abb9da936443b263b9232813695fd3e21124a
Daniel Chia <dchia00@citymail.cuny.edu>
2015-12-07 19:14:24 -0500

    fixed bad redirects on login on news app

--- site/news/models.py
+++ site/news/models.py
@@ -3,6 +3,10 @@ from datetime import datetime
 
 #in order to create urls, newspaper and categories can be slugified
 class Article(models.Model):
+    """
+    Model to store all the article data coming from the RSS feeds
+    obtained through feedparser
+    """
     #Newspaper | Category | Title | Link | Description
     newspaper = models.CharField(max_length=20) 
     category = models.CharField(max_length=15)
@@ -15,12 +19,19 @@ class Article(models.Model):
         return self.title
 
 class Image(models.Model):
+    """
+    Image model to grab image from a particular url and attach it to an article in the db
+    will store image source and main article text
+    """
     # article_id | images_url
     article_id=models.IntegerField()
     image_url=models.URLField()
     main_text=models.TextField(null=True, blank=True)
 
 class Bookmark(models.Model):
+    """
+    Bookmark model to store saved articles based on user id
+    """
     # user_id | article_id
     user_id=models.IntegerField()
     article_id=models.IntegerField()
