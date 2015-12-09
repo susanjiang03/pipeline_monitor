d52d0cfafd42058651764772b628950da3aa561b
Daniel Chia <dchia00@citymail.cuny.edu>
2015-12-09 18:59:43 -0500

    added FT for user input on bad url

---
+++ site/news/tests.py
@@ -0,0 +1,222 @@
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.contrib.auth.models import AnonymousUser, User
+
+from news.views import index, filterfeeds, userfeeds, newspaper
+from news.models import Article, Image
+
+#news page
+class NewsPageTest(TestCase):
+
+    #checks that /news/ goes to index view
+    def test_news_url_resolves_to_index_view(self):
+        found = resolve('/news/')
+        self.assertEqual(found.func, index)
+
+    #checks that index view has the index html template
+    def test_news_page_returns_correct_html(self):
+        request = HttpRequest()
+        request.user = AnonymousUser()
+        response = index(request)
+        expected_html = render_to_string('index.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    #seems like I have to create the articles manually
+    #every damn time
+    def test_article_appears_on_news_page(self):
+        article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
+
+        response = self.client.get('/news/')
+        self.assertContains(response, 'Technology')
+        self.assertContains(response, 'New York Times')
+
+    def test_Can_redirect_to_pipeline(self):
+        response = self.client.get('/news/')
+        self.assertContains(response, 'See pipeline')
+
+#article model
+class AritcleModeltest(TestCase):
+
+    def test_saving_and_retrieving_articles(self):
+
+        #article has title | url | description | newspaper | category
+        article_1 = Article()
+        article_1.newspaper = "Python Daily"
+        article_1.category = "Programming"
+        article_1.title = "Python in 10 minutes"
+        article_1.url = "http://realpython.com"
+        article_1.description = "Learn the Python programming language in 10 minutes"
+        article_1.save()
+
+        article_2 = Article()
+        article_2.newspaper = "NYT"
+        article_2.category = "Programming"
+        article_2.title = "Kung Fu React"
+        article_2.url = "http://flask.com"
+        article_2.description = "Best tutorial to learning react on the web"
+        article_2.save()
+
+        saved_articles = Article.objects.all()
+        self.assertEqual(saved_articles.count(), 2)
+
+        self.assertEqual(article_1, Article.objects.all()[0])
+        self.assertEqual(article_1.newspaper, 'Python Daily')
+        self.assertEqual(article_1.category, 'Programming')
+        self.assertEqual(article_1.title, 'Python in 10 minutes')
+        self.assertEqual(article_1.url, "http://realpython.com")
+        self.assertEqual(article_1.description, "Learn the Python programming language in 10 minutes")
+
+        self.assertEqual(article_2, Article.objects.all()[1])
+        self.assertEqual(article_2.newspaper, "NYT")
+        self.assertEqual(article_2.category, "Programming")
+        self.assertEqual(article_2.title, "Kung Fu React")
+        self.assertEqual(article_2.url, "http://flask.com")
+        self.assertEqual(article_2.description, "Best tutorial to learning react on the web")
+
+class NewspaperViewTest(TestCase):
+
+    #bad test because, the create of the article newspaper does
+    #not directly affect the url creation, no relation
+    def test_uses_newspaper_template(self):
+        article = Article.objects.create()
+        article.newspaper = "New York Times"
+        response = self.client.get('/news/newspaper/nytimes/')
+        self.assertTemplateUsed(response, 'newspaper.html')
+
+    #can't populate pages for some reason
+    def test_displays_article_from_that_newspaper(self):
+        #create 2 aritcles from different newspaper
+        #newspaper, title, url, description, category
+        correct_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
+
+        response = self.client.get('/news/newspaper/nytimes/')
+        
+        self.assertContains(response, correct_article.newspaper)
+        self.assertContains(response, correct_article.category)
+        self.assertNotContains(response, "Los Angeles Times")
+        self.assertNotContains(response, "Westside gang")
+
+
+class FilterFeedsTest(TestCase):
+
+    def test_uses_filterfeeds_template(self):
+        response = self.client.get('/news/filterfeeds')
+        self.assertTemplateUsed(response, 'filterfeeds.html')
+
+    def test_displays_a_newspaper_block(self):
+        an_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
+        response = self.client.get('/news/filterfeeds')
+        
+        self.assertContains(response, an_article.newspaper)
+
+    def test_displays_a_category(self):
+        an_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
+        response = self.client.get('/news/filterfeeds')
+        
+        self.assertContains(response, an_article.category)
+
+#can't test for content
+#that's an integration as it requires click selection of content
+class UserFeedsTest(TestCase):
+    
+    def test_uses_userfeeds_template(self):
+        response = self.client.get('/news/userfeeds')
+        self.assertTemplateUsed(response, 'userfeeds.html')
+
+
+class ArticleImagesTest(TestCase):
+
+    def test_uses_images_template(self):
+        #have to create article, as images belong to an article id
+        an_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
+
+        response = self.client.get('/news/images/' + str(an_article.id))
+        self.assertTemplateUsed(response, 'image_text.html')
+
+    def test_has_article_attributes(self):
+
+        an_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Technology")
+
+        response = self.client.get('/news/images/' + str(an_article.id))
+
+        #test that newspaper, category and title display on page
+        self.assertContains(response, an_article.newspaper)
+        self.assertContains(response, an_article.category)
+        self.assertContains(response, an_article.title)
+
+class CategoryTemplateTest(TestCase):
+
+    #verify the news/category/{category}/ page will only show the articles for this category for this newspaper
+    def test_category_template(self):
+        #create an article
+        an_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Fashion")
+
+        response = self.client.get('/news/category/' + an_article.category + '/')
+        self.assertContains(response, an_article.category)
+        self.assertContains(response, an_article.title)
+        self.assertContains(response, an_article.url)
+        self.assertNotContains(response, "Denver Post")
+        self.assertNotContains(response, "Sports")
+        self.assertTemplateUsed(response, 'category.html')
+
+class MainImageTest(TestCase):
+
+    #Verify the main and test show on news/images/(article_id) page  if there is an image and main text extraced
+    def test_image_text_tempalte(self):
+        #create an article
+        an_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Fashion")
+
+        image_text = Image.objects.create(article_id=an_article.id, image_url="http://helloworld.jpg", main_text="hello world")
+
+        response = self.client.get('/news/images/' + str(an_article.id))
+
+        #test thte page
+        self.assertContains(response, an_article.newspaper)
+        self.assertContains(response, an_article.category)
+        self.assertContains(response, an_article.title)
+        
+        image_url = Image.objects.filter(article_id=an_article.id)[0].image_url
+        main_text = Image.objects.filter(article_id=an_article.id)[0].main_text
+        
+        self.assertContains(response, image_url)
+        self.assertContains(response, main_text)
+
+    #verify if the article don't have image and text, don't show the link for view image and text
+    def test_no_image_text(self):
+        #create an article
+        an_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.flask.org", description="I don't know", category="Fashion")
+
+        response = self.client.get('/news/')
+        
+        #test thte page
+        self.assertContains(response, an_article.newspaper)
+        self.assertNotContains(response, an_article.description)
+        self.assertContains(response, an_article.url)
+        self.assertNotContains(response, "View Top Image and Main Text")
+
+
+class AllArticlesTest(TestCase):
+
+    #verify the allarticles will only show the article that has iamge and text
+    def test_all_article_template(self):
+        #create two article, one has image, the other don't
+        one_article = Article.objects.create(newspaper="New York Times", title="Adele is back", url="www.nytimes.com", description="I don't know", category="Fashion")
+        one_image_text = Image.objects.create(article_id=one_article.id, image_url="http://helloworld.jpg", main_text="hello world")
+
+        second_article = Article.objects.create(newspaper="Denver Post", title="Barnes & Noble to close in downtown Denver", url="www.flask.org", description="I don't know", category="Fashion")
+
+         #test thte page
+        response = self.client.get('/news/allimages')
+    
+        self.assertContains(response, one_article.title)
+        self.assertContains(response, one_image_text.image_url)
+        self.assertContains(response, one_image_text.main_text)
+        self.assertContains(response, one_article.newspaper)
+        self.assertNotContains(response, second_article.title)
+        self.assertNotContains(response, second_article.url)
+        self.assertTemplateUsed(response, 'allarticles.html')
+
+
+
---
+++ tndo/server.py
@@ -0,0 +1,140 @@
+import os
+import sys
+
+sys.path.insert(0, '../site')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "core.settings"
+)
+
+import django
+
+django.setup()
+
+import tornado.ioloop
+import tornado.web
+from tornado import websocket
+from pmonitor.models import Task, Status
+from json import dumps
+from datetime import datetime, date
+
+__author__ = 'jhohman'
+
+GLOBALS = dict(
+    sockets=[]
+)
+
+
+class Pipeline(object):
+    LINK_WIDTH = 40
+    GROUP = 1
+
+    def __init__(self):
+        self.status = dict(
+            nodes=[],
+            links=[]
+        )
+        self.num_tasks = 0
+
+    def add_task(self, task):
+        self.status['nodes'].append(dict(
+            group=self.GROUP,
+            task_id=task.task_id,
+            name=task.name,
+            description=task.description,
+            status=Status.TRANS[task.status],
+            message=task.message,
+            url=task.url if task.url else "#",
+            last_run=task.last_run
+        ))
+
+        self.num_tasks += 1
+        if self.num_tasks > 1:
+            self.status['links'].append(dict(
+                source=self.num_tasks - 2,
+                target=self.num_tasks - 1,
+                value=self.LINK_WIDTH
+            ))
+
+    def json_response(self):
+        dt_handler = (
+            lambda obj: obj.strftime('%m/%d/%Y %I:%M:%S %P')
+            if isinstance(obj, datetime) else None
+        )
+        return dumps(self.status, default=dt_handler)
+
+
+CACHED_PIPELINE = Pipeline()
+
+
+def get_task_status():
+    task = Task.objects.filter(parent=None)[0]
+    yield task
+    while True:
+        try:
+            task = task.get_next_task()
+        except Task.DoesNotExist:
+            raise StopIteration()
+        else:
+            yield task
+
+
+def build_pipeline():
+    pipeline = Pipeline()
+    for task in get_task_status():
+        pipeline.add_task(task)
+
+    return pipeline
+
+
+def iter_line():
+    global CACHED_PIPELINE
+    pipeline = build_pipeline()
+
+    json_status = pipeline.json_response()
+    old_json_status = CACHED_PIPELINE.json_response()
+
+    if json_status != old_json_status:
+        CACHED_PIPELINE = pipeline
+        print '\n' + json_status + '\n'
+        for socket in GLOBALS['sockets']:
+            socket.write_message(json_status)
+
+
+class ClientSocket(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        GLOBALS['sockets'].append(self)
+        print "WebSocket opened"
+        pipeline = build_pipeline()
+        json_status = pipeline.json_response()
+        print "push pipeline"
+        self.write_message(json_status)
+
+    def on_close(self):
+        print "WebSocket closed"
+        GLOBALS['sockets'].remove(self)
+
+
+class Announcer(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        data = self.get_argument('data')
+        for socket in GLOBALS['sockets']:
+            socket.write_message(data)
+
+        print 'Posted data: %s' % data
+        self.write('Posted data: %s' % data)
+
+
+application = tornado.web.Application([
+    (r"/socket", ClientSocket),
+    (r"/push", Announcer),
+])
+
+if __name__ == "__main__":
+    application.listen(8081)
+    main = tornado.ioloop.IOLoop.instance()
+    periodic = tornado.ioloop.PeriodicCallback(iter_line, 2000, io_loop=main)
+    periodic.start()
+    main.start()
