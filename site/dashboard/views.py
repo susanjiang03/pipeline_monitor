28bd8fba1fca5a60b5dbea265c72938ca77abd56
Susan Jiang <susanjiang03@gmail.com>
2015-12-10 00:24:36 -0500

    fix bug to shows stats in each page in dashboard app

--- site/dashboard/views.py
+++ site/dashboard/views.py
@@ -13,101 +13,6 @@ from django.http import HttpResponse
 the percentage for analysis, each valid rss url can link to  the dashboard for the artciles populated from this rss url'''
 
 
-def total_summary(request):
-    template = 'dash_base.html'
-    #initialize
-    feedurls=[]
-    total_articles = 0
-    total_images = 0
-    total_text = 0
-    pct_of_images = 0
-    pct_of_text = 0
-    
-    #get the array of rss url from RSSurl and number of valid and invalid rssurl
-    query = RSSurl.objects.all().distinct()
-    num_urls = len(query)
-    valid_query = RSSurl.objects.filter(valid=True)
-    valid_urls = len(valid_query)
-    pct_valid = valid_urls/float(num_urls) *100
-    invalid_urls = num_urls - valid_urls
-    
-    #loop through the arrary
-    for each in query:
-        url = each.rss_url.encode('UTF-8')
-        valid = each.valid
-        articlequery = Article.objects.filter(rssurl_id=each.id)
-        #initialize
-        number_of_articles = len(articlequery)
-        number_of_images = 0
-        number_of_text= 0
-        pct_of_images = 0
-        pct_of_text = 0
-        newspaper = ""
-        category = ""
-        
-        #get the artcles for this rss url
-        for article in articlequery:
-            newspaper = article.newspaper
-            category = article.category
-            image = Image.objects.filter(article_id=article.id)
-            
-            if image :
-                # get the stat of image
-                if image[0].image_url :
-                    number_of_images += 1
-                        # get the stat of main_text
-                if image[0].main_text :
-                    number_of_text += 1
-    
-        #pct for each rss url
-        if number_of_articles==0:
-            pct_of_images = 0
-            pct_of_text = 0
-        else:
-           pct_of_images = number_of_images/float(number_of_articles) *100
-           pct_of_text = number_of_text/float(number_of_articles)
-        
-        total_articles += number_of_articles
-        total_images += number_of_images
-        total_text += number_of_text
-        
-        feedurls.append({
-                        "rssurl_id":each.id,
-                        "url": url,
-                        "valid": valid,
-                        "newspaper": newspaper,
-                        "category": category,
-                        "number_of_articles": number_of_articles,
-                        "number_of_images": number_of_images,
-                        "number_of_text" : number_of_text,
-                        "pct_of_images": round(pct_of_images,2),
-                        "pct_of_text": round(pct_of_images,2)
-                        })
-    #calculate the total pct
-    if total_articles==0:
-        pct_total_images = 0
-        pct_total_text = 0
-        pct_articles = 0
-    else:
-        pct_total_images = total_images/float(total_articles) *100
-        pct_total_text   = total_text/float(total_articles) *100
-        pct_articles = total_articles/float(len(query)*10) * 100
-    summary={
-    "num_urls": num_urls,
-        "valid_urls": valid_urls,
-            "invalid_urls": invalid_urls,
-                "pct_valid": round(pct_valid,2),
-                    "total_articles": total_articles,
-                        "pct_articles": round(pct_articles,2),
-                            "total_images": total_images,
-                                "pct_total_images": round(pct_total_images,2),
-                                    "total_text": total_text,
-                                        "pct_total_text": round(pct_total_text,2),
-                                        }
-    
-    return render(template, {'summary': summary})
-
-
 def index(request):
     
     template = 'dash_index.html'
@@ -188,7 +93,7 @@ def index(request):
        pct_total_images = total_images/float(total_articles) *100
        pct_total_text   = total_text/float(total_articles) *100
        pct_articles = total_articles/float(len(query)*10) * 100
-    summary={
+    summary = {
            "num_urls": num_urls,
            "valid_urls": valid_urls,
            "invalid_urls": invalid_urls,
@@ -200,6 +105,7 @@ def index(request):
            "total_text": total_text,
            "pct_total_text": round(pct_total_text,2),
            }
+    request.session['summary'] = summary
 
     return render(request,template,{'feedurls':feedurls,'summary':summary})
 
@@ -209,6 +115,7 @@ def index(request):
 def rssurl(request,rssurl_id):
 
     template = 'rssfeed.html'
+    summary = request.session['summary']
     articles = []
     rssurl = RSSurl.objects.filter(id=rssurl_id)[0].rss_url.encode('UTF-8')
     newspaper = ""
@@ -245,13 +152,15 @@ def rssurl(request,rssurl_id):
             "text": text,
          })
 
-    return render(request,template,{'articles':articles,'rssurl':rssurl,'newspaper':newspaper,'category':category})
+    return render(request,template,{'articles':articles,'rssurl':rssurl,
+                  'newspaper':newspaper,'category':category,'summary':summary})
 
 
 '''this will read info for all aritlces'''
 def articles(request):
     
     template = 'articles.html'
+    summary = request.session['summary']
     articles = []
     validrssurl = RSSurl.objects.filter(valid=True)
   
@@ -291,7 +200,7 @@ def articles(request):
                          "text": text,
                                     })
 
-    return render(request,template,{'articles':articles})
+    return render(request,template,{'articles':articles,'summary':summary})
 
 
 '''download save all data to csv file'''
@@ -334,8 +243,9 @@ def downloadall(request):
 '''view all images in database'''
 def images(request):
     template = 'images.html'
+    summary = request.session['summary']
     imagequery = Image.objects.all().distinct()
-    return render(request,template,{'images':imagequery})
+    return render(request,template,{'images':imagequery,'summary':summary})
 
 
 
